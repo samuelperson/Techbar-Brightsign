e711e0d2455f0240aaa5317a569f260dd4c82b02
samuelperson <solielgo@gmail.com>
2023-06-22 10:15:22 -0500

    Fixed clock

--- Assets-with-BSN/Digital-Clock /dist/script.js
+++ Assets-with-BSN/Digital-Clock /dist/script.js
@@ -9,10 +9,13 @@ var clock = new Vue({
 var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 var timerID = setInterval(updateTime, 1000);
 updateTime();
-function updateTime() {
+function updateTime() { 
+    //The time is set at a +2 hours because some reason, brightsign sets the clock two hours behind. To make the time right, add +2 to getHours, so that the server is two hours ahead, but brightsign's time is right
+    
     var cd = new Date();
-    clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock.time = zeroPadding(cd.getHours() + 2, 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
     clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+
 };
 
 function zeroPadding(num, digit) {
